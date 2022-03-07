0c60cb85e90572cfc7f128eef7dff83c3a8869d3
sk0540 <suzaki9607@gmail.com>
2022-03-07 15:38:51 +0900

    Change how to load js file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,49 +1,42 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'nuxt-diff-test',
-    htmlAttrs: {
-      lang: 'ja'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'nuxt-diff-test',
+        htmlAttrs: {
+            lang: 'ja'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' }
+        ],
+        script: [{
+            src: "diff-match-patch.js",
+        }, ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    script: [
-      {
-        
-      }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [ 
-  ],
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {}
 }
